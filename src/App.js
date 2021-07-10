782432dbabe5b3cd454af4e17385ef5c7e5d4cc7
Biraj Poddar <biraj.poddar@delabs.co.in>
2021-07-10 12:17:13 +0530

    Logo addded

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
 import logo from './logo.svg';
-import './App.css';
+import rainbow from './rainbow.png';
+import rainbowLogo from './rainbowLogo.jpg';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return ( <div className="App">
+		<header className="App-header">
+			<img src={rainbowLogo}
+				className="App-logo"
+				alt="logo" />
+			<p>
+				Rainbow Film Production is coming shortly near a PC/Mobile
+			</p>
+		</header>
+	</div>
+	);
 }
 
 export default App;
